8cba99c4298681ef31b2a222fffa32aae68ec4e9
Ankita <b200101@nitsikkim.ac.in>
2023-06-30 11:04:42 +0530

    Modified Files

--- src/components/Codeboard.js
+++ src/components/Codeboard.js
@@ -1,63 +1,93 @@
 import React, { useState } from 'react';
-import './Codeboard.css'; // Import the CSS file
+import './Codeboard.css';
 
 const Codeboard = () => {
-  const [isInputVisible, setInputVisible] = useState(false);
-  const [output, setOutput] = useState('');
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+  const [codeInputValue, setCodeInputValue] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState('Language');
 
-  const handleInputChange = () => {
-    setInputVisible(!isInputVisible);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
 
   const handleRunCode = () => {
-    // Code execution logic here
-    // Set the output value based on the result
-    setOutput('Test cases or errors will be displayed here');
+    // Execute code logic here
+    // You can use the inputValue state to get the input value
+    // Update the outputValue state with the output of the code execution
+    // For demonstration purposes, let's assume the output is "Hello, World!"
+    setOutputValue('Hello, World!');
+  };
+
+  const handleToggleInput = () => {
+    setShowInput(!showInput);
+  };
+
+  const handleCodeInputChange = (event) => {
+    setCodeInputValue(event.target.value);
+  };
+
+  const handleLanguageSelect = (language) => {
+    setSelectedLanguage(language);
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-8">
-          <div className="editor-box">
-            <div className="editor-navbar">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" checked={isInputVisible} onChange={handleInputChange} />
-                <label className="form-check-label">Input</label>
-              </div>
-              <div className="btn-group">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Language
-                </button>
-                <ul className="dropdown-menu">
-                  <li><button className="dropdown-item" type="button">Option 1</button></li>
-                  <li><button className="dropdown-item" type="button">Option 2</button></li>
-                  <li><button className="dropdown-item" type="button">Option 3</button></li>
-                </ul>
-              </div>
-              <button className="btn btn-primary" type="button" onClick={handleRunCode}>Run</button>
-              <button className="btn btn-success" type="button">Download</button>
+    <div className="codeboard-container">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="navbar-left">
+          <div className="dropdown">
+            <button className="dropdown-btn">{selectedLanguage}</button>
+            <div className="dropdown-content">
+              {/* Language options */}
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('C')}>C</button>
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('C++')}>C++</button>
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('Python')}>Python</button>
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('JavaScript')}>JavaScript</button>
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('Java')}>Java</button>
+              <button className="dropdown-option" onClick={() => handleLanguageSelect('PHP')}>PHP</button>
             </div>
-            <textarea className="form-control editor-textarea" rows="10"></textarea>
           </div>
-          {isInputVisible && (
-            <div className="input-box mt-3">
-              <h5>Input:</h5>
-              <textarea className="form-control" rows="5"></textarea>
-            </div>
-          )}
+          <button className="navbar-btn">Download</button>
         </div>
-        <div className="col-lg-4">
-          <div className="output-box">
-            <h5>Output:</h5>
-            <div className="output-content">
-              <div className="output-result">{output}</div>
-            </div>
-          </div>
+        <div className="navbar-right">
+          <button className="navbar-btn" onClick={handleRunCode}>
+            Run
+          </button>
+        </div>
+      </nav>
+
+      {/* Code area */}
+      <div className="code-area">
+        <textarea
+          value={codeInputValue}
+          onChange={handleCodeInputChange}
+          placeholder="// Write your code here"
+          className="code-input"
+        />
+      </div>
+
+      {/* Input */}
+      <div className="input-container">
+        <label className="input-label">
+          <input type="checkbox" checked={showInput} onChange={handleToggleInput} />
+          Test using input
+        </label>
+        {showInput && (
+          <textarea className="input-box" value={inputValue} onChange={handleInputChange} placeholder="Enter input" style={{ height: '100%' }} />
+        )}
+      </div>
+
+      {/* Output */}
+      <div className="output-container">
+        <h2 className="output-title">Output</h2>
+        <div className="output-box">
+          <pre>{outputValue}</pre>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Codeboard;
